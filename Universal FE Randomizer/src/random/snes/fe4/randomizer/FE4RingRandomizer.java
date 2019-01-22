eeed6bd5f710b30456d6dbf267412e774da00910
DESKTOP-CCP6C7T\Lu <lushen124@gmail.com>
2019-01-21 18:04:49 -0800

    Removed Recover Ring from the list of randomizable rings.

--- Universal FE Randomizer/src/random/snes/fe4/randomizer/FE4RingRandomizer.java
+++ Universal FE Randomizer/src/random/snes/fe4/randomizer/FE4RingRandomizer.java
@@ -1,8 +1,10 @@
 package random.snes.fe4.randomizer;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 import fedata.snes.fe4.FE4Data;
 import random.snes.fe4.loader.ItemMapper;
@@ -12,7 +14,9 @@ public class FE4RingRandomizer {
 	static final int rngSalt = 8008;
 	
 	public static void randomizeRings(ItemMapper itemMap, Random rng) {
-		List<FE4Data.Item> ringList = new ArrayList<FE4Data.Item>(FE4Data.Item.rings);
+		Set<FE4Data.Item> ringSet = new HashSet<FE4Data.Item>(FE4Data.Item.rings);
+		ringSet.removeAll(FE4Data.Item.blacklistedRings);
+		List<FE4Data.Item> ringList = new ArrayList<FE4Data.Item>(ringSet); 
 		for (int index : itemMap.allIndices()) {
 			FE4Data.Item item = itemMap.getItemAtIndex(index);
 			if (item.isRing()) {
