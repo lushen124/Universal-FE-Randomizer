6d6e2d72e1562929ff2d7545e8e1b06372e59dd0
DESKTOP-CCP6C7T\Lu <lushen124@gmail.com>
2019-02-08 21:23:08 -0800

    0.7.6f (Final beta build for FE4 stuff)

--- Universal FE Randomizer/src/ui/MainView.java
+++ Universal FE Randomizer/src/ui/MainView.java
@@ -108,7 +108,7 @@ public class MainView implements FileFlowDelegate {
 		super();
 		
 		Shell shell = new Shell(mainDisplay, SWT.SHELL_TRIM & ~SWT.MAX); 
-		 shell.setText("Yune: A Universal Fire Emblem Randomizer (v0.7.6e)");
+		 shell.setText("Yune: A Universal Fire Emblem Randomizer (v0.7.6f)");
 		 shell.setImage(new Image(mainDisplay, Main.class.getClassLoader().getResourceAsStream("YuneIcon.png")));
 		 
 		 screenHeight = mainDisplay.getBounds().height;
