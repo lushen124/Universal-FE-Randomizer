3ffb97a9c7c0cd6508f592e05a21a17d5790fcb4
DESKTOP-CCP6C7T\Lu <lushen124@gmail.com>
2019-03-10 12:28:07 -0700

    Preparing for 0.8.1 release.

--- Universal FE Randomizer/src/random/snes/fe4/randomizer/FE4Randomizer.java
+++ Universal FE Randomizer/src/random/snes/fe4/randomizer/FE4Randomizer.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
