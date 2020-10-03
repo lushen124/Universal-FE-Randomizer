bb1d57d1686255468f53055d30e8129d29f95135
DESKTOP-CCP6C7T\Lu <lushen124@gmail.com>
2020-10-03 14:18:42 -0700

    Custom icons!

--- Universal FE Randomizer/src/application/Main.java
+++ Universal FE Randomizer/src/application/Main.java
@@ -3,6 +3,8 @@ package application;
 import org.eclipse.swt.widgets.*;
 
 import ui.MainView;
+import util.GBAImageCodec;
+import util.WhyDoesJavaNotHaveThese;
 
 public class Main {
 	
@@ -10,6 +12,13 @@ public class Main {
 	static MainView mainView;
 
 	public static void main(String[] args) {
+		
+		System.out.println("Earth Splitter: " + WhyDoesJavaNotHaveThese.displayStringForBytes(GBAImageCodec.getGBAGraphicsDataForImage("weaponIcons/EarthSplitter.png", GBAImageCodec.gbaWeaponColorPalette)));
+		System.out.println("Gust Shot: " + WhyDoesJavaNotHaveThese.displayStringForBytes(GBAImageCodec.getGBAGraphicsDataForImage("weaponIcons/GustShot.png", GBAImageCodec.gbaWeaponColorPalette)));
+		System.out.println("Fierce Flame: " + WhyDoesJavaNotHaveThese.displayStringForBytes(GBAImageCodec.getGBAGraphicsDataForImage("weaponIcons/FierceFlame.png", GBAImageCodec.gbaWeaponColorPalette)));
+		System.out.println("Dark Miasma: " + WhyDoesJavaNotHaveThese.displayStringForBytes(GBAImageCodec.getGBAGraphicsDataForImage("weaponIcons/DarkMiasma.png", GBAImageCodec.gbaWeaponColorPalette)));
+		System.out.println("Holy Light: " + WhyDoesJavaNotHaveThese.displayStringForBytes(GBAImageCodec.getGBAGraphicsDataForImage("weaponIcons/HolyLight.png", GBAImageCodec.gbaWeaponColorPalette)));
+		
 		 /* Instantiate Display object, it represents SWT session */
 		  mainDisplay = new Display();
 
