d6e05ed70b3ed17f7ac8a5b8bfc9e8f8ac1752b0
Geene <marvinvoss@outlook.de>
2023-01-08 15:12:16 +0100

    remove

--- Universal FE Randomizer/src/application/Main.java
+++ Universal FE Randomizer/src/application/Main.java
@@ -1,37 +1,15 @@
 package application;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
 import org.eclipse.swt.widgets.Display;
 
 import ui.MainView;
-import util.DebugListener;
-import util.DebugPrinter;
 
 public class Main {
 	
 	static Display mainDisplay;
 	static MainView mainView;
 
-	public static void main(String[] args) throws IOException {
-		
-		File f = new File("C:/users/Marvin/Desktop/Yune/log.txt");
-		BufferedWriter bw = new BufferedWriter(new FileWriter(f));
-		DebugPrinter.registerListener(new DebugListener() {
-			
-			@Override
-			public void logMessage(String category, String message) {
-				try {
-					bw.append(String.format("[%s] %s%n", category, message));
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		}, "sysout");
+	public static void main(String[] args) {
 		
 		 /* Instantiate Display object, it represents SWT session */
 		  mainDisplay = new Display();
@@ -46,8 +24,6 @@ public class Main {
 
 		  /* Dispose the display */
 		  mainDisplay.dispose();
-		  
-		  bw.close();
 	}
 
 }
