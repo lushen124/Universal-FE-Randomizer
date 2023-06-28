be3575d1eeb42d1b62266d482779b8ce97c6fb30
Geene <marvinvoss@outlook.de>
2023-06-28 20:36:12 +0200

    fix npe for fe6

--- Universal FE Randomizer/src/ui/views/RewardRandomizationView.java
+++ Universal FE Randomizer/src/ui/views/RewardRandomizationView.java
@@ -195,7 +195,7 @@ public class RewardRandomizationView extends YuneView<RewardOptions> {
             return new RewardOptions(rewardMode, randomRewardsButton.getSelection(), enemyDropsButton.getSelection() == true ? enemyDropChanceSpinner.getSelection() : 0);
         }
 
-        return new RewardOptions(randomizeChestVillageRewards.getSelection(), enemyDropsButton.getSelection() == true ? enemyDropChanceSpinner.getSelection() : 0);
+        return new RewardOptions(randomizeChestVillageRewards.getSelection(), !GameType.FE6.equals(type) && enemyDropsButton.getSelection() == true ? enemyDropChanceSpinner.getSelection() : 0);
     }
 
 }
